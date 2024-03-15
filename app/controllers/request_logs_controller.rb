12e89eb8d1e3e50e3233964632f816783eccfd84
Pawlo Kharkhalis <4854467+nomox@users.noreply.github.com>
2024-03-15 14:01:29 +0200

    Request logs controller: set filter data

--- app/controllers/request_logs_controller.rb
+++ app/controllers/request_logs_controller.rb
@@ -1,17 +1,21 @@
 class RequestLogsController < ApplicationController
-  def index
-    @methods         = RequestLog.distinct(:method)
-    @response_status = RequestLog.distinct(:response_status)
-    @error_classes   = RequestLog.distinct('error.class_name')
+  before_action :set_filter_data, only: %i[index]
 
-    @request_logs    = RequestLog.all.order(timestamp: :desc)
-    @request_logs    = RequestLogsQuery.new(search_params, @request_logs).call
-    @hit_stats       = @request_logs.collect_stats
+  def index
+    @request_logs = RequestLog.all.order(timestamp: :desc)
+    @request_logs = RequestLogsQuery.new(search_params, @request_logs).call
+    @hit_stats    = @request_logs.collect_stats
   end
 
   private
 
   def search_params
     params.permit(:method, :response_status, :error_class)
+  end
+
+  def set_filter_data
+    @methods         = RequestLog.distinct(:method)
+    @response_status = RequestLog.distinct(:response_status)
+    @error_classes   = RequestLog.distinct('error.class_name')
   end
 end
